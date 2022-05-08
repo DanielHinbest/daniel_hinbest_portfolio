2935210dbef6e04c40ad40458eee6b27f1ce0b7a
daniel hinbest <daniel.hinbest@proton.me>
2022-05-07 22:40:11 -0400

    carousel built

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Footer = () => (
+<footer className="container">
+    <p className="float-end"><a href="#">Back to top</a></p>
+    <p>&copy; 2017–2021 Company, Inc. &middot; <a>Privacy</a> &middot; <a>Terms</a></p>
+  </footer>
+);
+
+export default Footer;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './css/index.css';
+import './css/carousel.css';
+import './css/bootstrap.min.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
